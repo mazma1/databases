b2c15bce6af6d7d0f96813040a6e0e81ea6996b8
Mary Mazi <mazi.mary.o@gmail.com>
2018-10-23 20:06:17 +0100

    update firewall rules to include internal traffic and egress traffic

--- terraform/firewalls_config.tf
+++ terraform/firewalls_config.tf
@@ -31,3 +31,36 @@ resource "google_compute_firewall" "allow_private_ssh_icmp" {
   source_tags = ["public", "private"]
   target_tags   = ["private"]
 }
+
+resource "google_compute_firewall" "allow_internal" {
+  name          = "allow-internal"
+  description   = "Allow internal traffic on the network."
+  network       = "${google_compute_network.demo-vpc.self_link}"
+  allow {
+    protocol    = "tcp"
+    ports       = ["0-65535"]
+  }
+
+  allow {
+    protocol    = "udp"
+    ports       = ["0-65535"]
+  }
+
+  allow {
+    protocol    = "icmp"
+  }
+  source_tags = ["public", "private"]
+  target_tags   = ["private", "public"]
+}
+
+resource "google_compute_firewall" "allow_all_outbound" {
+  name               = "allow-all-outbound"
+  description        = "Allow all outbound connections access across the firewall of the Virtual Private Cloud."
+  direction          = "EGRESS"
+  network            = "${google_compute_network.demo-vpc.self_link}"
+  allow {
+    protocol    = "tcp"
+    ports       = ["80", "8080", "443"]
+  }
+  destination_ranges = ["0.0.0.0/0"]
+}
